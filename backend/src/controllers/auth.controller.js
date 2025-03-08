6d53891d63b444585a9a254eef334108284b5094
PiotrEliks <piotr.eliks@wp.pl>
2025-03-08 15:11:47 +0100

    Added login/logout function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,41 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+
 export const login = async (req, res) => {
-    res.send("Login route");
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({
+            where: {
+                email: email,
+            }
+        });
+
+        if (!user) {
+           return res.status(404).json({ message: "User not found"});
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        generateToken(user.id, res);
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.log("Error in login: ", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export const logout = async (req, res) => {
+    try {
+        res.cookie("token", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error in logout: ", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
-import User from "../models/User.model.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const getUsers = async (req, res) => {
     try {
@@ -16,10 +17,23 @@ export const addUser = async (req, res) => {
     try {
         const { fullName, email, password, role } = req.body;
 
+        const existingUser = await User.findOne({
+            where: {
+                email: email
+            }
+        });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
         const newUser = await User.create({
             fullName,
             email,
-            password,
+            password: hashedPassword,
             role
         });
 
@@ -63,7 +77,7 @@ export const updateUser = async (req, res) => {
                 id: userId,
             }
         });
-        
+
         res.status(200).json({ message: "User's informations have been updated"});
     } catch (error) {
         console.error("Error in updateUser: ", error);
