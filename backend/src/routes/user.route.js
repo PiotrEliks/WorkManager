9c605d06a30bd6f579115c3c0c5d0a7afb829a90
PiotrEliks <piotr.eliks@wp.pl>
2025-03-08 15:18:16 +0100

    Added middleware

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { addUser, deleteUser, getUsers, updateUser } from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/user/add", addUser);
-router.get("/all", getUsers);
-router.delete("/user/delete/:userId", deleteUser);
-router.put("/user/update/:userId", updateUser);
+router.post("/user/add", protectRoute, addUser);
+router.get("/all", protectRoute, getUsers);
+router.delete("/user/delete/:userId", protectRoute, deleteUser);
+router.put("/user/update/:userId", protectRoute, updateUser);
 
 export default router;
