1812eabac20823c5191bcec83698475be6de3fd5
PiotrEliks <piotr.eliks@wp.pl>
2025-03-12 15:00:19 +0100

    Added cron job

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,10 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import meterRoutes from "./routes/meter.route.js";
 import { connectDB } from "./lib/db.js";
+import nodemailer from "nodemailer";
+import cron from "node-cron";
+import Meter from "./models/meter.model.js";
+import { Sequelize, Op } from "sequelize";
 
 const app = express();
 
@@ -26,6 +30,52 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/meters", meterRoutes);
+
+const transporter = nodemailer.createTransport({
+  host: process.env.NODEMAILER_HOST,
+  port: process.env.NODEMAILER_PORT,
+  secure: false,
+  auth: {
+    user: process.env.NODEMAILER_EMAIL,
+    pass: process.env.NODEMAILER_PASSWORD,
+  },
+});
+
+async function sendNotificationEmail(meterName, email) {
+    try {
+        await transporter.sendMail({
+            from: process.env.NODEMAILER_EMAIL,
+            to: email,
+            subject: `${meterName} - Zbliża się termin przeglądu`,
+            text: "This is a notification from your device",
+        });
+        console.log("Message sent")
+    } catch (error) {
+        console.error("Error in sendNotification: ", error);
+    }
+};
+
+cron.schedule("* * * * *", async () => {
+    try {
+        const meters = await Meter.findAll({
+            where: {
+                [Op.or]: [
+                    { inspectionExpiryDate: { [Op.eq]: Sequelize.literal(`CURRENT_DATE + INTERVAL '1 day'`) }},
+                    { nextInspectionDate: { [Op.eq]: Sequelize.literal(`CURRENT_DATE + INTERVAL '1 day'`) }}
+                ]
+            }
+        });
+
+        meters.forEach(device => {
+            sendNotificationEmail(device.name, 'piotr.eliks@wp.pl');
+          });
+        console.log("Wykonano")
+    } catch (error) {
+        console.error("Error in cron.schedule: ", error);
+    }
+}, {
+    timezone: "Europe/Warsaw"
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
--- backend/src/lib/mailer.js
+++ backend/src/lib/mailer.js
@@ -1,5 +1,10 @@
 import nodemailer from "nodemailer";
+import cron from "node-cron";
+import Meter from "../models/meter.model.js";
+import { Sequelize, Op } from "sequelize";
 
+
+console.log(process.env.NODEMAILER_EMAIL)
 const transporter = nodemailer.createTransport({
   host: process.env.NODEMAILER_HOST,
   port: process.env.NODEMAILER_PORT,
@@ -10,10 +15,38 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const info = transporter.sendMail({
-    from: `"WorkManager" <${process.env.NODEMAILER_EMAIL}>`,
-    to: "piotr.eliks@wp.pl",
-    subject: "Hello ✔",
-    text: "Hello world?",
-    html: "<b>Hello world?</b>",
-  });
+async function sendNotificationEmail(meterName, email) {
+    try {
+        await transporter.sendMail({
+            from: process.env.NODEMAILER_EMAIL,
+            to: email,
+            subject: `${meterName} - Zbliża się termin przeglądu`,
+            text: "This is a notification from your device",
+        });
+        console.log("Message sent")
+    } catch (error) {
+        console.error("Error in sendNotification: ", error);
+    }
+};
+
+cron.schedule("* * * * *", async () => {
+    try {
+        const meters = await Meter.findAll({
+            where: {
+                [Op.or]: [
+                    { inspectionExpiryDate: { [Op.eq]: Sequelize.fn(`CURRENT_DATE + INTERVAL '1 day'`) }},
+                    { nextInspectionDate: { [Op.eq]: Sequelize.fn(`CURRENT_DATE + INTERVAL '1 day'`) }}
+                ]
+            }
+        });
+
+        meters.forEach(device => {
+            sendNotificationEmail(device.name, 'piotr.eliks@wp.pl');
+          });
+        console.log("Wykonano")
+    } catch (error) {
+        console.error("Error in cron.schedule: ", error);
+    }
+}, {
+    timezone: "Europe/Warsaw"
+});
