1e0a1f02df473f42e59a8a3c550555528a5d62c1
PiotrEliks <piotr.eliks@wp.pl>
2025-03-16 17:20:27 +0100

    Updated user model

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/user.model.js";
+import User from "../models/users.model.js";
 import Permission from "../models/permission.model.js";
 
 export const protectRoute = async (req, res, next) => {
