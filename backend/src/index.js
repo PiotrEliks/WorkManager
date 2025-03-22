3ccb37ffed93f3770d66b7bbf847d73de0022269
PiotrEliks <piotr.eliks@wp.pl>
2025-03-22 16:48:00 +0100

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://workmanager-xlsf.onrender.com",
+    //origin: "http://localhost:5173",
     credentials: true,
   })
 );
